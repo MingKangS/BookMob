dab333d4dc8dc509fea18431787f87ac1708f4cd
Siow Ming Kang <62229606+MingKangS@users.noreply.github.com>
2021-08-23 21:42:57 +0800

    Create Book model

--- api/models.py
+++ api/models.py
@@ -2,6 +2,13 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    username = models.CharField(null=False ,max_length=20, unique=True)
-    email = models.EmailField(null=False ,max_length=50, unique=True)
-    password = models.CharField(null=False, max_length=50)
+	username = models.CharField(null=False ,max_length=20, unique=True)
+	email = models.EmailField(null=False ,max_length=50, unique=True)
+	password = models.CharField(null=False, max_length=50)
+
+class Book(models.Model):
+	title = models.CharField(null=False ,max_length=40)
+	author = models.CharField(null=False ,max_length=40)
+	date_posted = models.DateField(null=False)
+	seller = models.ForeignKey(User, null=False, on_delete=models.CASCADE)
+
